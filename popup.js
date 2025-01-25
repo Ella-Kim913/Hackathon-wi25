dfba55230a3c18b626f5ab9c0abae251d146ba10
Kira Brodsky <kira.b@outlook.com>
2025-01-25 14:41:13 -0800

    alt text count appears onclick

--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
 document.getElementById("analyzeButton").addEventListener("click", () => {
     if (document.getElementById("power-button").style.backgroundColor == "green") {
         document.getElementById("power-button").style.backgroundColor = "#B50906";
+        document.getElementById("altTextCount").style.display = "none";
     } else {
         document.getElementById("power-button").style.backgroundColor = "green";
+        document.getElementById("altTextCount").style.display = "block";
 
     }
+
+    // if (document.getElementById("altTextCount").style.display == "none"){
+    //     document.getElementById("altTextCount").style.display = "block";
+    // } else {
+    //     document.getElementById("altTextCount").style.display == "none";
+    // }
     document.getElementById("power-button").style.boxShadow = "0 -2px -3px 0 rgba(0, 0, 0, 0.2), 0 -6px -7px 0 rgba(0, 0, 0, 0.19)";
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, { action: "startGeneratingAltText" }, (response) => {
