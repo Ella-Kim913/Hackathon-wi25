1ee6d6dbf0ee0bc8500e9fbe4e08787868671ba2
Kira Brodsky <kira.b@outlook.com>
2025-01-25 14:34:55 -0800

    button change color onclick

--- popup.js
+++ popup.js
@@ -1,7 +1,13 @@
 document.getElementById("analyzeButton").addEventListener("click", () => {
+    if (document.getElementById("power-button").style.backgroundColor == "green") {
+        document.getElementById("power-button").style.backgroundColor = "#B50906";
+    } else {
+        document.getElementById("power-button").style.backgroundColor = "green";
+
+    }
+    document.getElementById("power-button").style.boxShadow = "0 -2px -3px 0 rgba(0, 0, 0, 0.2), 0 -6px -7px 0 rgba(0, 0, 0, 0.19)";
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, { action: "startGeneratingAltText" }, (response) => {
-
             // if (response && response.score) {
             //     document.getElementById("scoreDisplay").innerText = `Score: ${response.score}`;
             // } else {
